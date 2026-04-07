dff883ddf146b8c3a22ec03a5e35e3934128e110
yasmin carolina <yaascarol@hotmail.com>
2026-04-07 19:47:10 -0300

    Update ItemPedido.java

--- src/main/java/com/seu/evento/api/model/ItemPedido.java
+++ src/main/java/com/seu/evento/api/model/ItemPedido.java
@@ -1,23 +1,31 @@
 package com.seu.evento.api.model;
 
 import jakarta.persistence.*;
-import lombok.Data;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotNull;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
 
 @Entity
-@Data
+@Getter
+@Setter
+@ToString(exclude = {"pedido", "ingresso"})
 public class ItemPedido {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @NotNull(message = "A quantidade é obrigatória")
+    @Min(value = 1, message = "A quantidade mínima é 1")
     private Integer quantidade;
 
     @ManyToOne
-    @JoinColumn(name = "pedido_id")
+    @JoinColumn(name = "pedido_id", nullable = false)
     private Pedido pedido;
 
     @ManyToOne
-    @JoinColumn(name = "ingresso_id")
+    @JoinColumn(name = "ingresso_id", nullable = false)
     private Ingresso ingresso;
 }
